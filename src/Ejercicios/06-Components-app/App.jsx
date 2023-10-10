96bf6797d4fe06b228f171f2e06c90ff84af1ffd
Marta Miras <martamirasmendez@gmail.com>
2023-10-10 12:32:39 +0200

    Ejercicio 8

---
+++ src/Ejercicios/06-Components-app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from '../06-Components-app/src/Hello.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+};
+
+export default App;
