63be767208f3d0b78c28fe24ba8a2590b2e834bd
Marta Miras <martamirasmendez@gmail.com>
2023-10-10 13:27:41 +0200

    Ejercicio24

--- src/Ejercicios/06-Components-app/src/Welcome.jsx
+++ src/Ejercicios/06-Components-app/src/Welcome.jsx
@@ -5,7 +5,7 @@ const Welcome = ({ name, age }) => {
   return (
     <div>
       <p>Welcome, {name}!</p>
-      {(age > 18 && age < 65) && <Age age={age} />}
+      {name === 'John' && age > 18 && age < 65 && <Age age={age} />}
     </div>
   );
 };
