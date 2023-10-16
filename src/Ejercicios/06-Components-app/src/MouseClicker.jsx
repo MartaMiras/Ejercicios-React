d768363f09a33a8018ab29433e20632d87315d45
Marta Miras <martamirasmendez@gmail.com>
2023-10-16 13:49:02 +0200

    Ejercicio38

--- src/Ejercicios/06-Components-app/src/MouseClicker.jsx
+++ src/Ejercicios/06-Components-app/src/MouseClicker.jsx
@@ -1,18 +1,40 @@
+// Agregue un controlador de eventos onClick a la imagen que imprima el src de la imagen
+// en la consola. ¿El atributo de nombre del botón todavía se imprime en la consola cuando
+// se hace clic en la imagen? ¿Por qué?
+
+// Si agregas un controlador de eventos onClick a la imagen que imprime el src
+// de la imagen en la consola, el atributo name del botón seguirá imprimiéndose en la
+// consola cuando se haga clic en la imagen. Esto se debe a cómo funcionan los eventos de
+// propagación en React y en el modelo de eventos del DOM.
+// Cuando haces clic en la imagen, se produce un evento de clic en la imagen,
+// y ese evento se propaga hacia arriba en el árbol DOM. Esto significa que el evento se
+// propaga primero al elemento padre más cercano, que es el botón en este caso. Por lo tanto,
+// si el botón tiene un controlador de eventos onClick, se ejecutará antes que el controlador
+// de eventos de la imagen, y el atributo name del botón se imprimirá en la consola antes de
+// que se imprima el src de la imagen.
+
+//con controlador de eventos
+
 import React from 'react';
 
-function MouseClicker({ name }) {
-  const handleClick = (event) => {
-    // Access the name attribute of the button
-    console.log(`Button clicked: ${event.currentTarget.getAttribute('name')}`);
+function MouseClicker({ name, imageSrc }) {
+  const handleButtonClick = () => {
+    console.log(`Button clicked: ${name}`);
+  };
+
+  const handleImageClick = () => {
+    console.log(`Image clicked: ${imageSrc}`);
   };
 
   return (
     <div>
-      <button name={name} onClick={handleClick}>
+      <button name={name} onClick={handleButtonClick}>
         Click me
         <img
-          src="image.jpg"  // Replace with your image
-          style={{ marginLeft: '5px' }}  // Add some spacing between the text and image
+          src={imageSrc}
+          alt="A descriptive alt text"
+          style={{ marginLeft: '5px' }}
+          onClick={handleImageClick} // Event handler for the image
         />
       </button>
     </div>
@@ -20,3 +42,8 @@ function MouseClicker({ name }) {
 }
 
 export default MouseClicker;
+
+// Con esta modificación, el controlador de eventos handleImageClick imprimirá el src de la imagen
+// en la consola cuando hagas clic en la imagen, mientras que el controlador de eventos
+// handleButtonClick imprimirá el name del botón cuando hagas clic en cualquier parte del botón,
+// incluida la imagen. La propagación de eventos es la razón por la cual ambos controladores se ejecutan.
