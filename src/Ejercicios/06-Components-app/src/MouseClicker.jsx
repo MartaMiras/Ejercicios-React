19325df47033ff69a8d40229c76c082e6fe29097
Marta Miras <martamirasmendez@gmail.com>
2023-10-16 13:44:55 +0200

    Ejercicio37

--- src/Ejercicios/06-Components-app/src/MouseClicker.jsx
+++ src/Ejercicios/06-Components-app/src/MouseClicker.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 function MouseClicker({ name }) {
   const handleClick = (event) => {
-    // Access the event.target.name property to get the name prop
-    console.log(`Button clicked: ${event.target.name}`);
+    // Access the name attribute of the button
+    console.log(`Button clicked: ${event.currentTarget.getAttribute('name')}`);
   };
 
   return (
     <div>
       <button name={name} onClick={handleClick}>
         Click me
+        <img
+          src="image.jpg"  // Replace with your image
+          style={{ marginLeft: '5px' }}  // Add some spacing between the text and image
+        />
       </button>
     </div>
   );
