e62b3b0fa8e1c158f565db5adab0b665b6ead8df
Marta Miras <martamirasmendez@gmail.com>
2023-10-22 14:08:50 +0200

    Ejercicio66

--- src/Ejercicios/FocusableInput.js
+++ src/Ejercicios/FocusableInput.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useRef } from 'react';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 
 const FocusableInput = () => {
   const inputRef = useRef(null);
 
   useEffect(() => {
-    // Focus the input element 
+    // Focus the input element
     if (inputRef.current) {
       inputRef.current.focus();
     }
   }, []);
 
   return (
-    <input
-      ref={inputRef}
-      type="text"
-      placeholder="Focusable Input"
-    />
+    <Container>
+      <Form.Control
+        ref={inputRef}
+        type="text"
+        placeholder="Focusable Input"
+      />
+    </Container>
   );
 };
 
