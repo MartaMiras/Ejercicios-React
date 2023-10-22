38091c3af2bbe220c5097ee1d510e86101e2d7e0
Marta Miras <martamirasmendez@gmail.com>
2023-10-22 15:53:30 +0200

    Ejercicio85

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Welcome from './Ejercicios/06-Components-app/src/Welcome';
 import GithubUser from './GithubUser';
+import React from 'react';
+import ShowGithubUser from './Ejercicios/ShowGithubUser'; 
+import LoginForm from './Ejercicios/LoginForm'; 
+import TodoList from './Ejercicios/TodoList';
 
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome name="YourName" />} />
-        <Route path="users/:username" element={<ShowGithubUser />} />
-      </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users/someUsername">Show Github User</Link>
+            </li>
+            <li>
+              <Link to="/login">Login Form</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Welcome name="YourName" />} />
+          <Route path="users/:username" element={<ShowGithubUser />} />
+          <Route path="login" element={<LoginForm />} />
+          <Route path="todo" element={<TodoList />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 };
-
