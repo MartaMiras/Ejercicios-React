d3c7a53ac521caa33f9997facc3c3956ad1e333c
Marta Miras <martamirasmendez@gmail.com>
2023-10-22 13:31:09 +0200

    Ejercicio57

--- src/Ejercicios/TodoList.jsx
+++ src/Ejercicios/TodoList.jsx
@@ -1,37 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function TodoList() {
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
 
-  const handleAddItem = () => {
-    if (newItem.trim() !== '') {
-      setItems([...items, newItem]);
-      setNewItem('');
-    }
-  };
+  function addItem() {
+    const newItem = document.getElementById("newItem").value;
+    setItems([...items, newItem]);
+    document.getElementById("newItem").value = "";
+  }
 
   return (
     <div>
-      <h2>Todo List</h2>
       <ul>
         {items.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
       </ul>
-      <div>
-        <input
-          type="text"
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-          placeholder="new item"
-        />
-        <button onClick={handleAddItem}>Add</button>
-      </div>
+      <input type="text" id="newItem" />
+      <button onClick={addItem}>Add Item</button>
     </div>
   );
 }
-
-export default TodoList;
-
-
