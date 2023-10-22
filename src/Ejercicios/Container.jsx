8fa9213c9b24115dc2306d74e3468fbbcba6061e
Marta Miras <martamirasmendez@gmail.com>
2023-10-22 14:24:06 +0200

    Ejercicio69

--- src/Ejercicios/Container.jsx
+++ src/Ejercicios/Container.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './container.css';
 
 function Container({ title, children }) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
-    <div className="container">
-      {title && <h2 className="container-title">{title}</h2>}
+    <div className={`container ${collapsed ? 'collapsed' : ''}`}>
+      <h2 className="container-title" onClick={toggleCollapse}>
+        {title}
+      </h2>
       {children}
     </div>
   );
